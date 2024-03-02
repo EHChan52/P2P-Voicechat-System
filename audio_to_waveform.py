720dc116ca3e3551092d51d774bddcba2fa435c5
BenWuyp <wuyipeng.ben@gmail.com>
2024-03-02 23:00:59 +0800

    update

--- audio_to_waveform.py
+++ audio_to_waveform.py
@@ -22,20 +22,16 @@ def Generate_waveform(filename):
         sample_rate = audio_file.getframerate()
         num_samples = audio_file.getnframes()
         plt.clf()
-        # Read the audio samples as a numpy array
         audio_samples = np.frombuffer(audio_file.readframes(num_samples), dtype=np.int16)
 
-        # Create a time array based on the sample rate and number of samples
         time = np.arange(0, num_samples) * (1.0 / sample_rate)
 
-        # Plot the audio waveform
         plt.plot(time, audio_samples)
         plt.xlabel('Time (seconds)')
         plt.ylabel('Amplitude')
         plt.title('Audio Waveform')
         plt.grid(False)
 
-        # Save the waveform plot as an image file
         plt.savefig('temp_plot.png')
         resize_image('temp_plot.png', 'temp_plot.png', (400, 200))
         im = Image.open("temp_plot.png")
